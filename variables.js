d8417fd3b07f4ee50dddd45e2182aca008056d46
keren charles <kerencharles@kerens-MacBook-Air.local>
2022-01-19 14:33:58 -0500

    first git commit

---
+++ variables.js
@@ -0,0 +1,9 @@
+let country = "usa";
+const continent = "north america";
+let population = '329,500,000';
+console.log(
+  `I live in the ${country}, which is in ${continent}, and it has ${population} people`
+);
+
+let haiti = 'haiti is cool'
+console.log(haiti)
